338e7f9304a44084a1e95d9ff50544b60be4760c
Go <gjgj821@gmail.com>
2018-09-28 20:14:00 +0800

    Update AndroidBug5497Workaround.java

--- src/android/AndroidBug5497Workaround.java
+++ src/android/AndroidBug5497Workaround.java
@@ -1,11 +1,18 @@
 package org.apache.cordova.labs.keyboard;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Rect;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
+
+import java.lang.reflect.Method;
 
 public class AndroidBug5497Workaround {
 
@@ -59,5 +66,51 @@ public class AndroidBug5497Workaround {
         Log.d("Rect",r.toString());
         return (r.bottom);
     }
-
+    /**
+     * 获取是否存在NavigationBar
+     * @param context
+     * @return
+     */
+    public boolean hasNavigationBar(Context context) {
+        boolean hasNavigationBar = false;
+        Resources rs = context.getResources();
+        int id = rs.getIdentifier("config_showNavigationBar", "bool", "android");
+        if (id > 0) {
+            hasNavigationBar = rs.getBoolean(id);
+        }
+        try {
+            Class systemPropertiesClass = Class.forName("android.os.SystemProperties");
+            Method m = systemPropertiesClass.getMethod("get", String.class);
+            String navBarOverride = (String) m.invoke(systemPropertiesClass, "qemu.hw.mainkeys");
+            if ("1".equals(navBarOverride)) {
+                hasNavigationBar = false;
+            } else if ("0".equals(navBarOverride)) {
+                hasNavigationBar = true;
+            }
+        } catch (Exception e) {
+        }
+        return hasNavigationBar;
+    }
+    /**
+     * 获取虚拟功能键高度
+     * @param context
+     * @return
+     */
+    public int getNavigationBarHeight(Context context) {
+        int vh = 0;
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics dm = new DisplayMetrics();
+        try {
+            @SuppressWarnings("rawtypes")
+            Class c = Class.forName("android.view.Display");
+            @SuppressWarnings("unchecked")
+            Method method = c.getMethod("getRealMetrics", DisplayMetrics.class);
+            method.invoke(display, dm);
+            vh = dm.heightPixels - windowManager.getDefaultDisplay().getHeight();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return vh;
+    }
 }
